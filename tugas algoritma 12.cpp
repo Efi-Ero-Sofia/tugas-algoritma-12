c626fcfb062af155d64798b833c08dd8429598b0
It's_Sofia <118363923+Efi-Ero-Sofia@users.noreply.github.com>
2023-02-05 12:10:18 +0700

    Create tugas algoritma 12.cpp

---
+++ tugas algoritma 12.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//tugas pertemuan ke 12
+
+int main(){
+
+string siswa [6]={"Efi ero sofia","jorgensio","Eko Ari","Ucup","ricka","mawar"};
+for(int a=0; a<6; a++){
+cout<<siswa[a]<<"\n";
+
+}
+
+}
